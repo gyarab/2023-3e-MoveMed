247c2933ef56b51de2104d43f1c8fc3e5e217757
Vlada45 <94285852+Vlada45@users.noreply.github.com>
2024-02-16 15:19:05 +0100

    Vytrénování AI

--- app/src/main/java/com/example/aplikaceprochronickpacienty/roomDB/UzivatelDao.kt
+++ app/src/main/java/com/example/aplikaceprochronickpacienty/roomDB/UzivatelDao.kt
@@ -52,6 +52,9 @@ interface UzivatelDao {
     @Query("UPDATE uzivatel_table SET WeightDayKG = :vaha WHERE SubjectId = :subjectId AND Date = :date")
     suspend fun updateWeight(subjectId: Int, date: String, vaha: Double): Int
 
+    @Query("SELECT EnergyIntakeDayKJ FROM uzivatel_table WHERE SubjectId = :subjectId AND Date = :date")
+    suspend fun getCalories(subjectId : Int, date : String): Double
+
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun addUser(uzivatel: Uzivatel)
 
