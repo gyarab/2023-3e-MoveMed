e741682dd5308c1b4f8bde486cdf39eb849d44ec
Vlada45 <94285852+Vlada45@users.noreply.github.com>
2024-04-25 16:40:28 +0200

    Update Message.kt

--- app/src/main/java/com/example/aplikaceprochronickpacienty/models/Message.kt
+++ app/src/main/java/com/example/aplikaceprochronickpacienty/models/Message.kt
@@ -1,5 +1,8 @@
 package com.example.aplikaceprochronickpacienty.models
 
+/** Citace datové třídy, která slouží k reprezentaci zpráv v aplikaci a umožňuje snadnou práci s daty zpráv.
+    Třída byla převzata z tohoto zdroje.
+    Zdroj: https://github.com/veeyaarVR/dialogflow_android_kotlin/blob/master/app/src/main/java/com/vignesh/chatbotkotlin/models/Message.kt **/
 data class Message(
     var message: String,
     var isReceived: Boolean
